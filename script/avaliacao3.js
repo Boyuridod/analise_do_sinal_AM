c79515f588d4901135aadbed4a232ff8652b6d70
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2024-06-11 08:23:20 -0300

    Variaveis inicializadas

---
+++ script/avaliacao3.js
@@ -0,0 +1,4 @@
+let E0 = getElementById("id1"), fm = getElementById("id2"), fm10 = getElementById("id3"),
+f0 = getElementById("id4"), f010 = getElementById("id5"), u = (getElementById("id6") * 2) / E0,
+f = getElementById("id7"), f10 = getElementById("id8"), f0_2 = getElementById("id9"), f0_2_10 = getElementById("id10"),
+f_2 = getElementById("id11"), f_2_10 = getElementById("id12"), f0_3 = getElementById("id13"), fo_3_10 = getElementById("id14");
