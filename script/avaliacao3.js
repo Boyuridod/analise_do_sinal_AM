a761d65961692683a742b4d36779f447661bbfe6
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2024-06-12 08:42:56 -0300

    FIX javascript

--- script/avaliacao3.js
+++ script/avaliacao3.js
@@ -1,14 +1,34 @@
-var E0 = getElementById("id1"), fm = getElementById("id2"), fm10 = getElementById("id3");
-var f0 = getElementById("id4"), f010 = getElementById("id5"), u = (getElementById("id6") * 2) / E0;
-var f = getElementById("id7"), f10 = getElementById("id8"), f0_2 = getElementById("id9"), f0_2_10 = getElementById("id10");
-var f_2 = getElementById("id11"), f_2_10 = getElementById("id12"), f0_3 = getElementById("id13"), fo_3_10 = getElementById("id14");
+var E0 = 0, fm = 0, fm10 = 0, f0 = 0, f010 = 0, u = 0, f = 0, f10 = 0, f0_2 = 0, f0_2_10 = 0, f_2 = 0, f_2_10 = 0, f0_3 = 0, fo_3_10 = 0;
 
- // Cria o elemento parágrafo
- var paragrafo = document.createElement("p");
-            
- // Define o texto do parágrafo
- paragrafo.textContent = "Este é um parágrafo adicionado dinamicamente.";
- 
- // Adiciona o parágrafo à div com o ID area_respostas
- var areaRespostas = document.getElementById("area_respostas");
- areaRespostas.appendChild(paragrafo);
+function recebeInputs() {
+
+    try {
+        E0 = document.getElementById("id1").value;
+        fm = document.getElementById("id2").value;
+        fm10 = document.getElementById("id3").value;
+        f0 = document.getElementById("id4").value;
+        f010 = document.getElementById("id5").value;
+        u = (document.getElementById("id6").value * 2) / E0;
+        f = document.getElementById("id7").value;
+        f10 = document.getElementById("id8").value;
+        f0_2 = document.getElementById("id9").value;
+        f0_2_10 = document.getElementById("id10").value;
+        f_2 = document.getElementById("id11").value;
+        f_2_10 = document.getElementById("id12").value;
+        f0_3 = document.getElementById("id13").value;
+        fo_3_10 = document.getElementById("id14").value;
+
+        calcular();
+    }
+
+    catch (error) {
+        alert(error);
+    }
+
+}
+
+function calcular() {
+
+    alert("deu bom" + E0);
+
+}
