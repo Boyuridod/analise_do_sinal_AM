61cee1263e1d0c7c8cb903325a9fb6d12784c56e
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2024-06-20 17:45:50 -0300

    ..

--- script/avaliacao3.js
+++ script/avaliacao3.js
@@ -1,19 +1,3 @@
-<<<<<<< Updated upstream
-var E0 = getElementById("id1"), fm = getElementById("id2"), fm10 = getElementById("id3");
-var f0 = getElementById("id4"), f010 = getElementById("id5"), u = (getElementById("id6") * 2) / E0;
-var f = getElementById("id7"), f10 = getElementById("id8"), f0_2 = getElementById("id9"), f0_2_10 = getElementById("id10");
-var f_2 = getElementById("id11"), f_2_10 = getElementById("id12"), f0_3 = getElementById("id13"), fo_3_10 = getElementById("id14");
-
- // Cria o elemento parágrafo
- var paragrafo = document.createElement("p");
-            
- // Define o texto do parágrafo
- paragrafo.textContent = "Este é um parágrafo adicionado dinamicamente.";
- 
- // Adiciona o parágrafo à div com o ID area_respostas
- var areaRespostas = document.getElementById("area_respostas");
- areaRespostas.appendChild(paragrafo);
-=======
 var E0 = 0, fm = 0, fm10 = 0, f0 = 0, f010 = 0, u = 0, fm2 = 0, fm2_10 = 0, f0_2 = 0, f0_2_10 = 0, fm3 = 0, fm3_10 = 0, f0_3 = 0, f0_3_10 = 0;
 var Em = 0;
 
@@ -130,4 +114,3 @@ function calcular() {
     respostas.appendChild(paragrafo);
 
 }
->>>>>>> Stashed changes
