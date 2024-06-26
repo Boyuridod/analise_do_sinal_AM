fbe8acb61f8c82d81c93e86fc477b7b93b6167bc
Matheus Henrique Maia Ramos <matheus.henrimr@gmail.com>
2024-06-25 22:58:33 -0300

    feat. New validation System

--- script/avaliacao3.js
+++ script/avaliacao3.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
             f0_2_10 = parseFloat(document.getElementById("id9").value);
 
             u = (parseFloat(document.getElementById("id10").value) * 2) / E0;
-            
+
             fm3 = parseFloat(document.getElementById("id11").value);
             fm3_10 = parseFloat(document.getElementById("id12").value);
             f0_3 = parseFloat(document.getElementById("id13").value);
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     analise.respostaMens = "";
                 else if (analise.respostaPort == "Corretas")
                     analise.respostaPort = "";
-                showAlert("Erro de Validação!", analise.respostaMens + "\n" + analise.respostaPort);
+                showAlert("Erro de Validação!", analise.respostaMens + "\n\n" + analise.respostaPort);
             }
         } catch (error) {
             showAlert("Erro!", "Erro ao receber os dados\n" + error);
@@ -53,28 +53,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function validar() {
         let respostaMens = 'Verifique se você digitou corretamente os ';
-        let respostaPort = 'Verifique também se você digitou corretamente os ';
+        let respostaPort = 'Verifique se você digitou corretamente os ';
 
-        if (fm != fm2 || fm != fm3 || fm2 != fm3) {
-            respostaMens += 'dados da frequência da mensageira';
-            if (fm10 != fm2_10 || fm10 != fm3_10 || fm2_10 != fm3_10) {
-                respostaMens += ' e os valores das potências';
+        if (fm == fm2 && fm2 == fm3 && fm3 == fm4) {
+            if(fm10 == fm2_10 && fm2_10 == fm3_10 && fm3_10 == fm4_10) {
+                respostaMens = 'Corretas';
+            } else {
+                respostaMens += 'valores das potências da mensageira';
             }
-        } else if (fm10 != fm2_10 || fm10 != fm3_10 || fm2_10 != fm3_10) {
-            respostaMens += 'valores das potências da mensageira';
         } else {
-            respostaMens = "Corretas";
+            respostaMens += 'dados da frequência da mensageira';
+            if(!(fm10 == fm2_10 && fm2_10 == fm3_10 && fm3_10 == fm4_10)) {
+                respostaMens += ' e os valores das potências dela';
+            } 
         }
 
-        if (f0 != f0_2 || f0 != f0_3 || f0_2 != f0_3) {
-            respostaPort += 'dados da frequência da portadora';
-            if (f010 != f0_2_10 || f010 != f0_3_10 || f0_2_10 != f0_3_10) {
-                respostaPort += ' e os valores das potências';
+        if (f0 == f0_2 && f0_2 == f0_3 && f0_3 == f0_4) {
+            if (f010 == f0_2_10 && f0_2_10 == f0_3_10 && f0_3_10 == f0_4_10) {
+                respostaPort = 'Corretas';
+            } else {
+                respostaPort += 'valores das potências da portadora';
             }
-        } else if (f010 != f0_2_10 || f010 != f0_3_10 || f0_2_10 != f0_3_10) {
-            respostaPort += 'valores das potências da portadora';
         } else {
-            respostaPort = "Corretas";
+            respostaPort += 'dados da frequência da mensageira';
+            if (!(f010 == f0_2_10 && f0_2_10 == f0_3_10 && f0_3_10 == f0_4_10)) {
+                respostaPort += ' e os valores das potências dela';
+            }
         }
 
         return {
